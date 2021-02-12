06682e298e636c234b931f756f117ac8fa72d0cf
sepehrmousaviyan <mousaviyan.sepehr@gmail.com>
2021-02-12 22:52:38 +0330

    first

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+        final int lenghX = 4;
+        final int lenghY = 4;
+        System.out.print("\u250C");
+        System.out.print("\u2500");
+        System.out.print("\u2500");
+        System.out.print("\u2500");
+        System.out.print("\u2500");
+        System.out.print("\u252C");
+        System.out.print("\n");
+        System.out.print("\u2502");
+        System.out.print("             ");
+        System.out.print("\n");
+        System.out.print("\u251C");
+        System.out.print("\u2500");
+        System.out.print("\u2500");
+        System.out.print("\u2500");
+        System.out.print("\u2500");
+//        String[][] Table = new String [lenghX][lenghY];
+//        for(int j = 0; j < lenghY; j++){
+//            for(int i = 0; i < lenghY; i++){
+//                if(j == 0){
+//                    if(i == 0){
+//                        Table[i][j] = "\u250C";
+//                    }
+//                    else if(i % 6 == 5){
+//
+//                    }
+//                    else{
+//                        System.out.print("\u2500");
+//                        System.out.print("\u2500");
+//                        System.out.print("\u2500");
+//                        System.out.print("\u2500");
+//                    }
+//                    System.out.print("\u2500");
+//                    System.out.print("\u2500");
+//                    System.out.print("\u2500");
+//                    System.out.print("\u2500");
+//                    System.out.print("\u2500");
+//                    System.out.print("\u2500");
+//                    System.out.print("\u252C");
+//                }
+//            else if(j == lenghY - 1){
+//
+//            }
+//            else{
+//
+//            }
+//            }
+//
+//        }
+    }
+}
